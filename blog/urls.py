f735179022354ca6181b48439f5e740141638c7b
Milin Mestry <milinmestry@gmail.com>
2017-02-19 00:22:46 +0530

    Created django form for New/edit blog post. Added Links to add/edit blog form only for authenticated user.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_form, name='post_form'),
+    url(r'^post/(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +12,31 @@ def post_detail(request, id):
     # Post.objects.get(pk=id)
     post = get_object_or_404(Post, pk=id)
     return render(request, 'blog/post_detail.htm', {'post': post})
+
+def post_form(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish_date = timezone.now()
+            post.save()
+            return redirect('post_detail', id=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.htm', {'form': form})
+
+def post_edit(request, id):
+    # Post.objects.get(pk=id)
+    post = get_object_or_404(Post, pk=id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish_date = timezone.now()
+            post.save()
+            return redirect('post_detail', id=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.htm', {'form': form, 'edit': 1})
